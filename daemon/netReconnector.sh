00c35023175a3b5fb3a68fe535896a186de876bc
¨ourzhuce¨ <ourzhuce@163.com>
2016-08-25 20:54:48 +0800

    Modify the check script: netConnector.sh

--- daemon/netReconnector.sh
+++ daemon/netReconnector.sh
@@ -4,10 +4,10 @@ source /etc/profile
 
 NetCard=wlan0	 		# the netcard connected to the internet
 PingTarget=192.168.31.1 	# the ping target, router ip or website, etc: 192.168.1.1, www.baidu.com
-LogFile=./daemon/NetReconnector.log	# log file
+LogFile=/home/pi/daemon/NetReconnector.log	# log file
 LogAll=true			# log msg no matter whether the net is connected, used to be check whether the script is running.
 
-test -e $LogFile || touch $LogFile
+test -e $LogFile || sudo touch $LogFile
 
 time=$(date "+%Y/%m/%d %H:%M:%S")
 
@@ -18,6 +18,7 @@ if [ "$ret" = "0" -o -z "$ret" ]; then
     sudo ifdown wlan0 && sudo ifup wlan0
 else
     if [ "$LogAll" = "true" ]; then
+        #echo "Network is ok, ret = $ret, $time" | tee -a $LogFile
         echo "Network is ok, ret = $ret, $time" | tee -a $LogFile
     fi
 fi
