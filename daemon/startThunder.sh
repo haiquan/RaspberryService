ccb9d374c1ab2a25c3b4c1fc6504efefca00d540
ourzhuce <ourzhuce@163.com>
2017-03-11 12:10:32 +0800

    update thunder script

--- daemon/startThunder.sh
+++ daemon/startThunder.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
-/home/pi/thunder/portal
+#/home/pi/thunder/portal
+#exit
+LogFile=/home/pi/daemon/thunder.log
 
-exit
+ps -A | grep EmbedThunderMa
+if [ $? != 0 ]
+then
+    echo 'need to run thunder' | tee -a $LogFile
+    sh /home/pi/daemon/startThunder.sh >> /tmp/checkdaemon.txt
+else
+    #echo 'thunder is running' >> /tmp/checkdaemon.txt
+    echo 'thunder is running'
+fi
